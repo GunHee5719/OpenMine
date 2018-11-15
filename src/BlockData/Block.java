248f83052786d9cde4035a40aa6ba4361c358da0
gunhee <ghj5719@naver.com>
2018-11-15 19:14:01 +0900

    System Model apply

---
+++ src/BlockData/Block.java
@@ -0,0 +1,9 @@
+package BlockData;
+
+/*
+ This class is for declaring the block's data. Various block classes will inherit this abstract class.
+ */
+
+public abstract class Block {
+
+}
---
+++ src/MineSweep/MineSweeping.java
@@ -0,0 +1,4 @@
+package MineSweep;
+
+public class MineSweeping {
+}
