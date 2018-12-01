06c97c7f911f884603d62213654e0c60c8b7ae00
JongminChoe <aaaa961224@gmail.com>
2018-12-02 08:35:18 +0900

    Add template matching function to detect type of each block

--- src/ImageProcessing/ImageProcessing.java
+++ src/ImageProcessing/ImageProcessing.java
@@ -1,5 +1,7 @@
 package ImageProcessing;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -31,9 +33,17 @@ public class ImageProcessing {
             }
         }
 
+        show(tmp, title);
+    }
+
+    public static void show(BufferedImage image) {
+        show(image, null);
+    }
+
+    public static void show(BufferedImage image, String title) {
         JFrame frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel(new ImageIcon(tmp)));
+        frame.add(new JLabel(new ImageIcon(image)));
         frame.pack();
         frame.setVisible(true);
     }
@@ -50,7 +60,10 @@ public class ImageProcessing {
     }
 
     public static void mark(BufferedImage image, Point point, int colorRGB) {
-        int size = 2;
+        mark(image, point, colorRGB, 2);
+    }
+
+    public static void mark(BufferedImage image, Point point, int colorRGB, int size) {
         for (int i = 1; i <= size; i++) {
             image.setRGB(point.x - i, point.y - i, colorRGB);
             image.setRGB(point.x + i, point.y - i, colorRGB);
@@ -61,19 +74,22 @@ public class ImageProcessing {
     }
 
     public static int[][] Binarize(BufferedImage image) {
+        return Binarize(image, 128);
+    }
+
+    public static int[][] Binarize(BufferedImage image, int threshold) {
         int width = image.getWidth();
         int height = image.getHeight();
 
         int[][] binarized = new int[width][height];
 
-        int rgb, red, green, blue, threshold;
+        int rgb, red, green, blue;
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 rgb = image.getRGB(x, y);
                 red = (rgb & 0xFF0000) >> 16;
                 green = (rgb & 0x00FF00) >> 8;
                 blue = rgb & 0x0000FF;
-                threshold = 128;
 
                 if (red < threshold && green < threshold && blue < threshold) {
                     binarized[x][y] = 0x00;
@@ -149,5 +165,84 @@ public class ImageProcessing {
         }
 
         return rectangles;
+    }
+
+    public static BufferedImage crop(BufferedImage src, Rectangle rect) {
+        return src.getSubimage(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    private static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
+
+    private static double compareImageAlpha(BufferedImage target, BufferedImage template) {
+        int width = template.getWidth();
+        int height = template.getHeight();
+
+        if (target.getWidth() != 18 || target.getHeight() != 18) {
+            target = resize(target, 18, 18);
+        }
+
+        double minDiff = Double.MAX_VALUE;
+        for (int shift_y = 0; shift_y <= target.getHeight() - height; shift_y++) {
+            for (int shift_x = 0; shift_x <= target.getWidth() - width; shift_x++) {
+                double diff = 0;
+                int comparedPixels = 0;
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int templateRGB = template.getRGB(x, y);
+                        if (((templateRGB & 0xFF000000) >> 24) == 0) {
+                            continue;
+                        }
+                        int templateRed = ((templateRGB & 0xFF0000) >> 16);
+                        int templateGreen = ((templateRGB & 0xFF00) >> 8);
+                        int templateBlue = templateRGB & 0xFF;
+
+                        int targetRGB = target.getRGB(x + shift_x, y + shift_y);
+                        int targetRed = ((targetRGB & 0xFF0000) >> 16);
+                        int targetGreen = ((targetRGB & 0xFF00) >> 8);
+                        int targetBlue = targetRGB & 0xFF;
+
+                        diff += Math.pow(targetRed - templateRed, 2) + Math.pow(targetGreen - templateGreen, 2) + Math.pow(targetBlue - templateBlue, 2);
+                        comparedPixels++;
+                    }
+                }
+                diff /= comparedPixels;
+                if (minDiff > diff) {
+                    minDiff = diff;
+                }
+            }
+        }
+
+        return minDiff;
+    }
+
+    public static int TemplateMatching(BufferedImage target, BufferedImage[] templates) {
+        double minDifference = Double.MAX_VALUE, difference;
+        int minIndex = templates.length;
+
+        for (int i = 0; i < templates.length; i++) {
+            difference = compareImageAlpha(target, templates[i]);
+            //System.out.println("difference with templates[" + i + "]: " + String.format("%.2f", difference));
+            if (minDifference > difference) {
+                minDifference = difference;
+                minIndex = i;
+            }
+        }
+
+		/*
+		if (maxSimilarity < 0.8) {
+			maxIndex = templates.length;
+		}
+		*/
+
+        return minIndex;
     }
 }
--- src/ImageProcessing/WindowDetect.java
+++ src/ImageProcessing/WindowDetect.java
@@ -2,18 +2,20 @@ package ImageProcessing;
 
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Toolkit;
+//import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+//import javax.swing.ImageIcon;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
 
 public class WindowDetect {
 
@@ -25,20 +27,21 @@ public class WindowDetect {
 
     // Test code
     public static void main(String[] args) {
-        // create test window
+		/* create test window
         try {
             JFrame frame = new JFrame(windowName);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.add(new JLabel(new ImageIcon(ImageIO.read(new File("minesweeper_window.png")))));
-            frame.setIconImage(Toolkit.getDefaultToolkit().getImage("minesweeper_icon.png"));
-            frame.pack();
-            frame.setVisible(true);
-            Thread.sleep(500);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+			frame.add(new JLabel(new ImageIcon(ImageIO.read(new File("minesweeper_window.png")))));
+			frame.setIconImage(Toolkit.getDefaultToolkit().getImage("minesweeper_icon.png"));
+	        frame.pack();
+	        frame.setVisible(true);
+	        Thread.sleep(500);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
 
         // initialize
         if (initWindowDetect() != 0) {
@@ -46,11 +49,7 @@ public class WindowDetect {
         }
 
         // show screen capture result
-        JFrame frame = new JFrame("Captured: " + windowName);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel(new ImageIcon(capturedScreen)));
-        frame.pack();
-        frame.setVisible(true);
+        ImageProcessing.show(capturedScreen, "Captured: " + windowName);
 
         // show processing result
         BufferedImage tmp = new BufferedImage(
@@ -66,21 +65,78 @@ public class WindowDetect {
             }
         }
 
-        frame = new JFrame("Detected: " + windowName);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel(new ImageIcon(tmp)));
-        frame.pack();
-        frame.setVisible(true);
+        ImageProcessing.show(tmp, "Detected: " + windowName);
+
+        // refresh
+        while (true) {
+            try {
+                // wait for enter key
+                System.out.println("Press Enter to refresh...");
+                System.in.read();
+                System.in.read();
+            } catch (IOException e) {
+            }
+            System.out.println("Refresh");
+
+			/* create test window
+	        try {
+	            JFrame frame = new JFrame(windowName);
+	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.add(new JLabel(new ImageIcon(ImageIO.read(new File("test.png")))));
+		        frame.pack();
+		        frame.setVisible(true);
+		        Thread.sleep(500);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	        */
+
+            HashMap<Point, Integer> changedBlocks = refresh();
+
+            // draw detected rectangles
+            tmp = new BufferedImage(
+                    capturedScreen.getColorModel(),
+                    capturedScreen.copyData(capturedScreen.getRaster().createCompatibleWritableRaster()),
+                    capturedScreen.getColorModel().isAlphaPremultiplied(),
+                    null); // clone capturedScreen
+
+            final int colors[] = {0x880015, 0xED1C24, 0xFF7F27, 0xFFF200, 0x22B14C, 0x00A2E8, 0x3F48CC, 0xA349A4, 0xFFFFFF, 0x7F7F7F};
+            for (Map.Entry<Point, Integer> entry : changedBlocks.entrySet()) {
+                int color = colors[entry.getValue()];
+
+                Rectangle targetRectangle = getBlockRectangle(entry.getKey());
+                targetRectangle.x += 2;
+                targetRectangle.y += 2;
+                targetRectangle.width -= 4;
+                targetRectangle.height -= 4;
+                ImageProcessing.drawRectangle(tmp, targetRectangle, color);
+
+                // additional rectangle
+                targetRectangle.x += 1;
+                targetRectangle.y += 1;
+                targetRectangle.width -= 2;
+                targetRectangle.height -= 2;
+                ImageProcessing.drawRectangle(tmp, targetRectangle, color);
+            }
+
+            ImageProcessing.show(tmp, "ReCaptured: " + windowName);
+            System.out.println(changedBlocks.size() + " blocks has been changed");
+        }
     }
 
     public static int initWindowDetect() {
+        // capture initial window
         capturedScreen = WindowCapture.captureWindow(windowName);
         if (capturedScreen == null) {
-            return 1;
+            return -1;
         }
 
+        // binarize image
         int[][] binarized = ImageProcessing.Binarize(capturedScreen);
 
+        // select biggest rectangle in labeled rectangles as a region of interest
         List<Rectangle> rectangles = ImageProcessing.Labeling(binarized);
         int size = 0;
         for (int i = 0; i < rectangles.size(); i++) {
@@ -91,21 +147,34 @@ public class WindowDetect {
             }
         }
 
-        columns = countVerticalLines(binarized);
-        rows = countHorizontalLines(binarized);
+        // cut and add padding in the binarized region of interest
+        int[][] binarizedRegionOfInterest = new int[regionOfInterest.width + 2][regionOfInterest.height + 2];
+        Arrays.fill(binarizedRegionOfInterest[0], 0xFF);
+        for (int x = 0; x < regionOfInterest.width; x++) {
+            binarizedRegionOfInterest[x + 1][0] = 0xFF;
+            System.arraycopy(binarized[regionOfInterest.x + x], regionOfInterest.y, binarizedRegionOfInterest[x + 1], 1, regionOfInterest.height);
+            binarizedRegionOfInterest[x + 1][regionOfInterest.height + 1] = 0xFF;
+        }
+        Arrays.fill(binarizedRegionOfInterest[regionOfInterest.width + 1], 0xFF);
+
+        // count number of blocks and adjust Zone of Interest
+        analyzeZoneOfInterest(binarizedRegionOfInterest);
+
+        // calculate a block size
         blockWidth = (double)regionOfInterest.width / columns;
         blockHeight = (double)regionOfInterest.height / rows;
 
-        // if block is not square
+        /* if a block is not square
         if (blockWidth / blockHeight > 1.02) {
-            blockHeight = blockWidth;
-            rows = (int)(regionOfInterest.height / blockHeight + 0.5); // round
-            blockHeight = (double)regionOfInterest.height / rows;
+        	blockHeight = blockWidth;
+        	rows = (int)(regionOfInterest.height / blockHeight + 0.5); // round
+        	blockHeight = (double)regionOfInterest.height / rows;
         } else if (blockHeight / blockWidth > 1.02) {
-            blockWidth = blockHeight;
-            columns = (int)(regionOfInterest.width / blockWidth + 0.5); // round
-            blockWidth = (double)regionOfInterest.width / columns;
+        	blockWidth = blockHeight;
+        	columns = (int)(regionOfInterest.width / blockWidth + 0.5); // round
+        	blockWidth = (double)regionOfInterest.width / columns;
         }
+        */
 
         // Debug
         System.out.println("Region of Interest: " + regionOfInterest.width + " x " + regionOfInterest.height + " pixels");
@@ -116,8 +185,68 @@ public class WindowDetect {
         return 0;
     }
 
+    public static HashMap<Point, Integer> refresh() {
+        HashMap<Point, Integer> changedBlocks = new HashMap<Point, Integer>();
+
+        // recapture window
+        BufferedImage recapturedScreen = WindowCapture.captureWindow(windowName);
+        if (recapturedScreen == null) {
+            return changedBlocks;
+        }
+
+        // read template images
+        int numberOfTemplate = 10;
+        BufferedImage[] templates = new BufferedImage[numberOfTemplate];
+        try {
+            for (int i = 0; i < numberOfTemplate; i++) {
+                templates[i] = ImageIO.read(new File("templates\\" + i + ".png"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                int changedPixels = 0;
+                Rectangle block = getBlockRectangle(x, y);
+                for (int block_y = block.y; block_y < block.y + block.height; block_y++) {
+                    for (int block_x = block.x; block_x < block.x + block.width; block_x++) {
+                        if (capturedScreen.getRGB(block_x, block_y) != recapturedScreen.getRGB(block_x, block_y)) {
+                            changedPixels++;
+                        }
+                    }
+                }
+                if (((double)changedPixels / (block.width * block.height)) > 0.36) {
+                    Rectangle targetRectangle = getBlockRectangle(x, y);
+
+                    BufferedImage targetImage = ImageProcessing.crop(recapturedScreen, targetRectangle);
+                    int templateMatchingResult = ImageProcessing.TemplateMatching(targetImage, templates);
+
+                    changedBlocks.put(new Point(x, y), templateMatchingResult);
+                    //System.out.println(String.format("%.2f", ((double)changedPixels / (block.width * block.height) * 100)) + "% of block (" + x + ", " + y + ") has changed to block #" + templateMatchingResult);
+                }
+            }
+        }
+
+        capturedScreen = recapturedScreen;
+
+        return changedBlocks;
+    }
+
+    public static Point getBlockPosition(Point p) {
+        return getBlockPosition(p.x, p.y);
+    }
+
     public static Point getBlockPosition(int x, int y) {
         return new Point((int)(regionOfInterest.x + blockWidth * (x + 0.5)), (int)(regionOfInterest.y + blockHeight * (y + 0.5)));
+    }
+
+    public static Rectangle getBlockRectangle(Point p) {
+        return getBlockRectangle(p.x, p.y);
+    }
+
+    public static Rectangle getBlockRectangle(int x, int y) {
+        return new Rectangle((int)(regionOfInterest.x + blockWidth * x + 0.5), (int)(regionOfInterest.y + blockHeight * y + 0.5), (int)(blockWidth + 0.5), (int)(blockHeight + 0.5));
     }
 
     public static int getColumns() {
@@ -128,28 +257,46 @@ public class WindowDetect {
         return rows;
     }
 
-    private static int countHorizontalLines(int[][] binarized) {
+    private static void analyzeZoneOfInterest(int[][] binarized) {
+        int width = binarized.length;
+        int height = binarized[0].length;
         HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> blacks = new HashMap<Integer, Integer>();
         boolean isBlack = false;
 
-        for (int x = regionOfInterest.x; x < regionOfInterest.x + regionOfInterest.width; x++) {
-            isBlack = binarized[x][regionOfInterest.y] == 0;
+        // do Vertical
+        for (int y = 0; y < height; y++) {
+            isBlack = binarized[0][y] == 0;
             int count = 0;
-            for (int y = regionOfInterest.y; y < regionOfInterest.y + regionOfInterest.height; y++) {
+            int black = 0;
+
+            for (int x = 0; x < width; x++) {
                 if (isBlack && binarized[x][y] == 0xFF) {
                     count++;
                     isBlack = false;
                 } else if (!isBlack && binarized[x][y] == 0) {
                     isBlack = true;
                 }
+
+                if (binarized[x][y] == 0 && count == 0) {
+                    black++;
+                }
             }
+
             if (counts.containsKey(count)) {
                 counts.replace(count, counts.get(count) + 1);
             } else {
                 counts.put(count, 1);
             }
+
+            if (blacks.containsKey(black)) {
+                blacks.replace(black, blacks.get(black) + 1);
+            } else {
+                blacks.put(black, 1);
+            }
         }
 
+        // count vertical lines
         int maxKey = 0;
         int maxValue = 0;
         Iterator<Integer> iterator = counts.keySet().iterator();
@@ -160,35 +307,63 @@ public class WindowDetect {
                 maxValue = counts.get(key);
             }
         }
+        columns = maxKey - 1;
 
-        return maxKey;
-    }
+        // calculate thickness of the leftmost vertical line
+        maxKey = 0;
+        maxValue = 0;
+        iterator = blacks.keySet().iterator();
+        while (iterator.hasNext()) {
+            int key = iterator.next();
+            if (maxValue < blacks.get(key)) {
+                maxKey = key;
+                maxValue = blacks.get(key);
+            }
+        }
+        regionOfInterest.x += (maxKey + 1) / 2;
+        regionOfInterest.width -= maxKey;
 
-    private static int countVerticalLines(int[][] binarized) {
-        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
-        boolean isBlack = false;
+        // initialize variables
+        counts.clear();
+        blacks.clear();
+        isBlack = false;
 
-        for (int y = regionOfInterest.y; y < regionOfInterest.y + regionOfInterest.height; y++) {
-            isBlack = binarized[regionOfInterest.x][y] == 0;
+        // do Horizontal
+        for (int x = 0; x < width; x++) {
+            isBlack = binarized[x][0] == 0;
             int count = 0;
-            for (int x = regionOfInterest.x; x < regionOfInterest.x + regionOfInterest.width; x++) {
+            int black = 0;
+
+            for (int y = 0; y < height; y++) {
                 if (isBlack && binarized[x][y] == 0xFF) {
                     count++;
                     isBlack = false;
                 } else if (!isBlack && binarized[x][y] == 0) {
                     isBlack = true;
                 }
+
+                if (binarized[x][y] == 0 && count == 0) {
+                    black++;
+                }
             }
+
             if (counts.containsKey(count)) {
                 counts.replace(count, counts.get(count) + 1);
             } else {
                 counts.put(count, 1);
             }
+
+            if (blacks.containsKey(black)) {
+                blacks.replace(black, blacks.get(black) + 1);
+            } else {
+                blacks.put(black, 1);
+            }
         }
 
-        int maxKey = 0;
-        int maxValue = 0;
-        Iterator<Integer> iterator = counts.keySet().iterator();
+        // count horizontal lines
+        maxKey = 0;
+        maxValue = 0;
+        iterator = counts.keySet().iterator();
         while (iterator.hasNext()) {
             int key = iterator.next();
             if (maxValue < counts.get(key)) {
@@ -196,8 +371,21 @@ public class WindowDetect {
                 maxValue = counts.get(key);
             }
         }
+        rows = maxKey - 1;
 
-        return maxKey;
+        // calculate thickness of the topmost horizontal line
+        maxKey = 0;
+        maxValue = 0;
+        iterator = blacks.keySet().iterator();
+        while (iterator.hasNext()) {
+            int key = iterator.next();
+            if (maxValue < blacks.get(key)) {
+                maxKey = key;
+                maxValue = blacks.get(key);
+            }
+        }
+        regionOfInterest.y += (maxKey + 1) / 2;
+        regionOfInterest.height -= maxKey;
     }
 
 }
